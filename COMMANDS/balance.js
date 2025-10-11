5dce486832559e4bcf8b403b02359416e44a2f78
Talmax <carlitosdiazplaza@gmail.com>
2025-10-11 19:29:43 -0400

    Change Text Test

--- COMMANDS/balance.js
+++ COMMANDS/balance.js
@@ -86,7 +86,7 @@ module.exports = {
                 .addFields(
                     { name: '\u200B', value: '━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━', inline: false },
                     { 
-                        name: 'Balance Overview', 
+                        name: 'Financial Overview', 
                         value: `\`\`\`yaml\nWallet: ${fmt(balance.wallet)}\nBank:   ${fmt(balance.bank)}\nTotal:  ${fmt(totalBalance)}\`\`\``, 
                         inline: false 
                     },
